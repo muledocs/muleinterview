ae9a3e0327e8dc7f868e811f042482c84b5ea091
muledocs <rafiqux84@gmail.com>
2025-04-18 14:40:29 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, NavLink, Navigate } from 'react-router-dom';
 import './App.css';
 import QuestionSearch from './QuestionSearch';
 import RosaPage from './pages/RosaPage';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/muleinterview">
       <div className="app">
         <nav>
           <ul>
             <li>
-              <NavLink to="/" end>Home</NavLink>
+              <NavLink to="/home">Home</NavLink>
             </li>
             <li>
-              <NavLink to="/rosa">ROSA</NavLink>
+              <NavLink to="/rosa">ROSA Questions</NavLink>
             </li>
           </ul>
         </nav>
 
         <Routes>
-          <Route path="/" element={<QuestionSearch />} />
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<QuestionSearch />} />
           <Route path="/rosa" element={<RosaPage />} />
         </Routes>
       </div>
